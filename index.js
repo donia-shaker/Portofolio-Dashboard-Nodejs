202443120c3f731f876743ef10f31e613c6a887f
unknown <donia.shaker11@gmail.com>
2022-06-05 07:09:29 -0400

    commit

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const Mywork = require("./public/js/mywork");
 const Landing = require("./public/js/landings");
 const landing = require("./public/js/landings");
 const landings = require("./public/js/landings");
+app.use(function(req, res, next) {
+  res.setTimeout(200);
+  next();
+});
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
